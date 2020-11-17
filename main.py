6ae03315c373acf755edb0310a78aad84d14d6d7
Ahmad N. F <fahmadnaufal@gmail.com>
2020-11-17 17:34:38 +0700

    latest updates: fix with current page layout

---
+++ main.py
@@ -0,0 +1,75 @@
+from bs4 import BeautifulSoup
+from theater import PurchasePeriod, Theater
+from datetime import datetime
+
+import requests
+import re
+
+base_url = "https://jkt48.com/"
+theater_path = "theater/schedule?lang=id"
+img_path = "/images/"
+
+img_teamj = "icon.team1.png"
+img_teamk = "icon.team2.png"
+img_teamt = "icon.team5.png"
+img_himawarigumi = "icon.team8.png"
+img_academy = "icon.team11.png"
+
+team_name = ["Academy Class A", "Team J", "Team KIII", "Team T", "Himawarigumi"]
+
+def parse_schedule(raw_tags):
+    columns = raw_tags[0].findAll('td')
+    title = str(columns[1].text)
+    date = datetime.strptime(columns[0].contents[2], "%d.%m.%Y").date()
+    show_time = datetime.strptime(columns[0].contents[-1].contents[0][-5:], "%H:%M")
+    ticket_time = datetime.strptime(columns[0].contents[-1].contents[2][-5:], "%H:%M")
+
+    # create schedule for purchase time
+    time_vip = PurchasePeriod.create_from_period_string("VIP", raw_tags[0].findAll('td')[3].getText())
+    time_ofc = PurchasePeriod.create_from_period_string("OFC", raw_tags[1].findAll('td')[1].getText())
+    time_gen = PurchasePeriod.create_from_period_string("General", raw_tags[2].findAll('td')[1].getText())
+
+    return Theater(title, date, show_time, ticket_time, [time_vip, time_ofc, time_gen])
+
+def parse_content(choice):
+    sch = []
+
+    img_selection = ""
+    if choice == 0:
+        img_selection = img_academy
+    elif choice == 1:
+        img_selection = img_teamj
+    elif choice == 2:
+        img_selection = img_teamk
+    elif choice == 3:
+        img_selection = img_teamt
+    elif choice == 4:
+        img_selection = img_himawarigumi
+    else:
+        pass
+
+    r = requests.get(base_url + theater_path)
+    fp = r.content
+
+    soup = BeautifulSoup(fp, 'html.parser')
+    rows = soup.find('table').findAll('tr')
+    schedules = [(rows[i], rows[i+1], rows[i+2]) for i in range(1, len(rows), 3)]
+    for schedule in schedules:
+        team_image = schedule[0].find('img').get('src')
+        if team_image.find(img_selection) != -1:
+            sch.insert(0, parse_schedule(schedule))
+
+    return sch
+
+def main():
+    for i in range(5):
+        print("%d. %s" % (i+1, team_name[i]))
+    choice = int(input("> ")) - 1
+
+    team_schedule = parse_content(choice)
+    print("Menampilkan %d Show untuk %s: \n" % (len(team_schedule), team_name[choice]))
+    for schedule in team_schedule:
+        print(schedule)
+
+if __name__ == '__main__':
+    main()
--- theater.py
+++ theater.py
@@ -2,16 +2,41 @@ from datetime import datetime
 from schedule import *
 
 class Theater(Schedule):
-    """docstring for Schedule"""
-    def __init__(self, raw_tag):
-        columns = raw_tag.findAll('td')
-        self.title = str(columns[1].text)
-        self.date = datetime.strptime(columns[0].contents[2], "%d.%m.%Y").date()
-        self.show_time = datetime.strptime(columns[0].contents[-1].contents[0][-5:], "%H:%M")
-        self.ticket_time = datetime.strptime(columns[0].contents[-1].contents[2][-5:], "%H:%M")
+    """docstring for Theater"""
+    def __init__(self, title, date, show_time, ticket_time, purchase_periods=[]):
+        self.title = title
+        self.date = date
+        self.show_time = show_time
+        self.ticket_time = ticket_time
+        self.purchase_periods = purchase_periods
 
-    def print_schedule(self):
-        return self.title + '\n' \
-                + self.date.strftime('%A, %d %B %Y') + '\n' \
-                + self.show_time.strftime('Show: %H:%M') + '\n' \
-                + self.ticket_time.strftime('Penukaran tiket: %H:%M') + '\n'
+    def __str__(self):
+        strep = self.title + '\n' \
+                    + self.date.strftime('%A, %d %B %Y') + '\n' \
+                    + self.show_time.strftime('Show: %H:%M') + '\n' \
+                    + self.ticket_time.strftime('Penukaran tiket: %H:%M') + '\n'
+
+        for period in self.purchase_periods:
+            strep += str(period) + '\n'
+
+        return strep
+
+class PurchasePeriod(object):
+    """docstring for PurchasePeriod"""
+    def __init__(self, id, title, start_time, end_time):
+        self.id = id
+        self.title = title
+        self.start_time = start_time
+        self.end_time = end_time
+
+    @classmethod
+    def create_from_period_string(cls, title, raw):
+        strtimes = raw.split(" - ")
+        start_time = datetime.strptime(" ".join(strtimes[0].split(" ")[1:3]), "%d.%m.%Y %H:%M")
+        end_time = datetime.strptime(" ".join(strtimes[1].split(" ")[1:3]), "%d.%m.%Y %H:%M")
+
+        return cls(0, title, start_time, end_time)
+
+    def __str__(self):
+        return self.title + ": " + self.start_time.strftime('%A, %d %B %Y %H:%M') + " - " + self.end_time.strftime('%A, %d %B %Y %H:%M')
+
